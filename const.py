73a00de4ac490a6089ff6e7c8f45ec36fb836187
Bogdan Stanea <adrianstanea1@gmail.com>
2023-12-12 22:43:03 +0200

    add comments and document code as well as algorithms
    Signed-off-by: Bogdan Stanea <adrianstanea1@gmail.com>

---
+++ const.py
@@ -0,0 +1,6 @@
+KERNEL_SIZE = 7                     # size of the kernel window used by SGML
+SIGMA = 2                           # standard deviation used in SGML byt the gaussian kernel
+PADDING = (KERNEL_SIZE - 1) // 2    # neccesary padding to keep the image size
+
+
+VERBOSE = True                      # print information about the process
