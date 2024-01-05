3495105cbbbc84b6df7b31bf746ee2802f47af71
Bogdan Stanea <adrianstanea1@gmail.com>
2024-01-05 19:53:08 +0200

    move hyperparameter to separate file

--- const.py
+++ const.py
@@ -1,3 +1,4 @@
+R = 3                               # window size using by SGML
 KERNEL_SIZE = 7                     # size of the kernel window used by SGML
 SIGMA = 2                           # standard deviation used in SGML byt the gaussian kernel
 PADDING = (KERNEL_SIZE - 1) // 2    # neccesary padding to keep the image size
